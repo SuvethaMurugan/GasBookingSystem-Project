f1a51f106dc7c4d563a3c02ab8b231909f83f685
Ganesh Babu <PGANES31@ford.com>
2024-02-21 10:53:09 +0530

    CustomerUpdate

--- src/main/java/com/companyname/GasBookingSystem/customer/CustomerControllerAdvice.java
+++ src/main/java/com/companyname/GasBookingSystem/customer/CustomerControllerAdvice.java
@@ -1,6 +1,8 @@
 package com.companyname.GasBookingSystem.customer;
 
 import com.companyname.GasBookingSystem.customer.Exception.CustomerException;
+import com.companyname.GasBookingSystem.customer.Exception.InvalidEmailException;
+import com.companyname.GasBookingSystem.customer.Exception.InvalidPasswordException;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
 import org.springframework.http.HttpStatus;
@@ -12,8 +14,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CustomerControllerAdvice {
     @ExceptionHandler(value={CustomerException.class})
-    public ResponseEntity<String> AccountException(CustomerException e){
+    public ResponseEntity<String> customerException(CustomerException e){
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
 
+    }
+    @ExceptionHandler(value={InvalidPasswordException.class})
+    public ResponseEntity<String> invalidPasswordException(InvalidPasswordException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value={InvalidEmailException.class})
+    public ResponseEntity<String> invalidEmailException(InvalidEmailException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
