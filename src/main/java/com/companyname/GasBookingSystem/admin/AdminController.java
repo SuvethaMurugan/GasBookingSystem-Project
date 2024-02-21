d41a16ea2b0a3fc53033b03e9dcbad8b47c633a4
Stalin K M <SSTALINK@ford.com>
2024-02-21 10:30:54 +0530

    admincommit

--- src/main/java/com/companyname/GasBookingSystem/admin/AdminController.java
+++ src/main/java/com/companyname/GasBookingSystem/admin/AdminController.java
@@ -1,8 +1,37 @@
 package com.companyname.GasBookingSystem.admin;
 
+import com.companyname.GasBookingSystem.admin.DTO.AdminLoginDTO;
+import com.companyname.GasBookingSystem.admin.Exception.AdminException;
+import com.companyname.GasBookingSystem.customer.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AdminController {
+    @Autowired
+    private AdminService adminService;
 
+    @GetMapping("/customers")
+    public List<Customer> customerList() {
+        return this.adminService.getAllCustomers();
+    }
+
+    @PostMapping("/login/adminId")
+    public Admin loginAdmin(@RequestBody AdminLoginDTO adminlogin) throws AdminException {
+        return this.adminService.loginAdminID(adminlogin.getAdminId(), adminlogin.getPassword());
+    }
+
+    @PostMapping("/login/emailId")
+    public Admin loginAdminEmail(@RequestBody AdminLoginDTO adminEmail) throws AdminException {
+        return this.adminService.loginAdminEmail(adminEmail.getEmailId(), adminEmail.getPassword());
+    }
+    @PostMapping("/logout")
+    public String adminLogout(@RequestBody AdminLoginDTO logout){
+        return this.adminService.adminLogout(logout.getEmailId(),logout.getPassword());
+    }
 }
--- src/main/java/com/companyname/GasBookingSystem/admin/AdminService.java
+++ src/main/java/com/companyname/GasBookingSystem/admin/AdminService.java
@@ -1,4 +1,14 @@
 package com.companyname.GasBookingSystem.admin;
 
+import com.companyname.GasBookingSystem.admin.Exception.AdminException;
+import com.companyname.GasBookingSystem.customer.Customer;
+
+import java.util.List;
+
 public interface AdminService {
+
+    List<Customer> getAllCustomers();
+    Admin loginAdminID (Integer adminId, String password)throws AdminException;
+    Admin loginAdminEmail (String email,String password)throws AdminException;
+    String adminLogout(String email,String password);
 }
