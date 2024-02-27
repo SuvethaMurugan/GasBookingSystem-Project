a275648eaf2f723a4a110e603f93041051a42a11
SuvethaMurugan <94278467+SuvethaMurugan@users.noreply.github.com>
2024-02-27 14:56:16 +0530

    BookingCommit

--- src/main/java/com/companyname/gasbookingsystem/admin/AdminServiceImpl.java
+++ src/main/java/com/companyname/gasbookingsystem/admin/AdminServiceImpl.java
@@ -1,28 +1,27 @@
 package com.companyname.gasbookingsystem.admin;
 
 import com.companyname.gasbookingsystem.admin.exception.AdminException;
+import com.companyname.gasbookingsystem.booking.Booking;
+import com.companyname.gasbookingsystem.booking.BookingRepository;
 import com.companyname.gasbookingsystem.customer.Customer;
 import com.companyname.gasbookingsystem.customer.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import com.companyname.gasbookingsystem.booking.Booking;
-import com.companyname.gasbookingsystem.booking.BookingRepository;
-
-import java.time.LocalDate;
-
 
 @Service
-public class AdminServiceImpl implements AdminService{
+public class AdminServiceImpl implements AdminService {
     @Autowired
     private CustomerRepository customerRepository;
     @Autowired
     private AdminRepository adminRepository;
     @Autowired
     private BookingRepository bookingRepository;
+
     @Override
     public List<Customer> getAllCustomers() {
         return this.customerRepository.findAll();
@@ -31,22 +30,23 @@ public class AdminServiceImpl implements AdminService{
     @Override
     public Admin loginAdminID(Integer adminId, String password) throws AdminException {
         Optional<Admin> adminLogin = adminRepository.findById(adminId);
-        if(adminLogin.isEmpty()) {
+        if (adminLogin.isEmpty()) {
             throw new AdminException("Login Credentials does not match");
         }
-        Admin admin=adminLogin.get();
-        if(admin.getPassword().equals(password))
+        Admin admin = adminLogin.get();
+        if (admin.getPassword().equals(password))
             return admin;
         else throw new AdminException("Login Credentials does not match");
     }
+
     @Override
-    public Admin loginAdminEmail (String email, String password) throws AdminException{
+    public Admin loginAdminEmail(String email, String password) throws AdminException {
         Optional<Admin> adminEmailLogin = adminRepository.findByEmail(email);
-        if(adminEmailLogin.isEmpty()){
+        if (adminEmailLogin.isEmpty()) {
             throw new AdminException("Login Credentials does not match");
         }
-        Admin admin=adminEmailLogin.get();
-        if(admin.getPassword().equals(password))
+        Admin admin = adminEmailLogin.get();
+        if (admin.getPassword().equals(password))
             return admin;
         else
             throw new AdminException("Login Credentials does not match");
@@ -56,6 +56,7 @@ public class AdminServiceImpl implements AdminService{
     public String adminLogout(String email, String password) {
         return "Logged Out Successfully";
     }
+
     @Override
     public List<Booking> getAllListOfCylinders() {
         return this.bookingRepository.findAllByDeliveryDate(LocalDate.now());
