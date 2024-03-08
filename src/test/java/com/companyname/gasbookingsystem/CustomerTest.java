f1cdbc9a8b4d84a821167c0a88a06ff46bdf0fa0
PGANES31 <65417+PGANES31@users.noreply.github.ford.com>
2024-03-08 11:04:59 +0530

    test

--- src/test/java/com/companyname/gasbookingsystem/CustomerTest.java
+++ src/test/java/com/companyname/gasbookingsystem/CustomerTest.java
@@ -21,50 +21,50 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class CustomerTest {
 
-        @Autowired
-        private CustomerService customerService;
-        Customer  customer;
+    @Autowired
+    private CustomerService customerService;
+    Customer  customer;
 
-        Address address;
-        MobileNumLoginDTO mobileNumLoginDTO;
-        UserNameLoginDTO userNameLoginDTO;
-        UpdateDTO updateDTO;
-        RegisterUserDTO registerUserDTO;
-        CylinderType cylinderType;
+    Address address;
+    MobileNumLoginDTO mobileNumLoginDTO;
+    UserNameLoginDTO userNameLoginDTO;
+    UpdateDTO updateDTO;
+    RegisterUserDTO registerUserDTO;
+    CylinderType cylinderType;
 
 
-        @BeforeEach
-        public void createCustomer(){
-            address=new Address(1,12,"StreetName","Chennai","6000001");
-            registerUserDTO=new RegisterUserDTO("Suvetha","Suvetha@123","suvetha@gmail.com","9489696937",address);
-            customer=Customer.builder().userName("Suvetha").password("Suvetha@123").mobileNo("9489696937").email("suvetha2003@gmail.com").address(address).build();
-            mobileNumLoginDTO = new MobileNumLoginDTO("9489696937", "Suvetha@123");
-            userNameLoginDTO = new UserNameLoginDTO("Suvetha", "Suvetha@123");
-            updateDTO = new UpdateDTO(1,"Prathiksaa","Suvetha@123","suvetha@gmail.com","9489696937",true,address);
+    @BeforeEach
+    public void createCustomer(){
+        address=new Address(1,12,"StreetName","Chennai","6000001");
+        registerUserDTO=new RegisterUserDTO("Suvetha","Suvetha@123","suvetha@gmail.com","9489696937",address);
+        customer=Customer.builder().userName("Suvetha").password("Suvetha@123").mobileNo("9489696937").email("suvetha2003@gmail.com").address(address).build();
+        mobileNumLoginDTO = new MobileNumLoginDTO("9489696937", "Suvetha@123");
+        userNameLoginDTO = new UserNameLoginDTO("Suvetha", "Suvetha@123");
+        updateDTO = new UpdateDTO(1,"Prathiksaa","Suvetha@123","suvetha@gmail.com","9489696937",true,address);
 
+    }
+    @AfterEach
+    public void DeleteValue(){
+        address=null;
+        registerUserDTO=null;
+        mobileNumLoginDTO = null;
+        userNameLoginDTO = null;
+        updateDTO = null;
+    }
+    @DisplayName("Customer Registration Test")
+    @Test
+    void CustomerRegistrationTest()
+    {
+
+        Cylinder cylinder=null;
+        Booking booking=null;
+        try {
+            Assertions.assertNotNull(customerService.registerUser(customer));
+
+        } catch (CustomerException | InvalidEmailException | InvalidPasswordException e) {
+            throw new RuntimeException(e);
         }
-        @AfterEach
-        public void DeleteValue(){
-            address=null;
-            registerUserDTO=null;
-            mobileNumLoginDTO = null;
-            userNameLoginDTO = null;
-            updateDTO = null;
-        }
-        @DisplayName("Customer Registration Test")
-        @Test
-        void CustomerRegistrationTest()
-        {
-
-            Cylinder cylinder=null;
-            Booking booking=null;
-            try {
-                Assertions.assertNotNull(customerService.registerUser(customer));
-
-            } catch (CustomerException | InvalidEmailException | InvalidPasswordException e) {
-                throw new RuntimeException(e);
-            }
-        }
+    }
     @DisplayName("Customer Registration Test")
     @Test
     void CustomerRegistrationWithMobileNumberGreaterThan10Test()
@@ -414,7 +414,7 @@ class CustomerTest {
     @DisplayName("Customer Test")
     @Test
     void getAllCylinderMedicalTest(){
-            Assertions.assertNotNull(customerService.getAllCylindersOfSpecifiedType(CylinderType.Medical));
+        Assertions.assertNotNull(customerService.getAllCylindersOfSpecifiedType(CylinderType.Medical));
     }
     @DisplayName("Customer Test")
     @Test
